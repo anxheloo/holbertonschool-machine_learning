92e5b6ea95b8ce751d3142ddfeb655a7c2d0c4ec
anxheloo <anxhelocenollari@gmail.com>
2023-02-23 06:25:50 -0800

    Transpose Matrix

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+'''Flip me over'''
 
 
-def matrix_transpose(matrix):    
+def matrix_transpose(matrix):
+    '''ca bone lale'''    
     result = [[matrix[j][i] for j in range(len(matrix))] for i in range (len(matrix[0]))]
     return result
 
