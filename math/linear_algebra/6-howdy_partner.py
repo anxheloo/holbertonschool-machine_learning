1e33c5e30a4e9a5d780a019955ebf999582004b6
anxheloo <anxhelocenollari@gmail.com>
2023-02-23 07:34:51 -0800

    Concatenate 2 Arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Concatenate 2 Strings'''
+
+
+def cat_arrays(arr1,arr2):
+    '''Add two Strings Together'''
+    newList = []
+    newList.extend(arr1)
+    newList.extend(arr2)
+    return newList
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [6, 7, 8]
+cat_arrays(arr1, arr2)
