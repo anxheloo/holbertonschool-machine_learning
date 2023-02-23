7c886d728023dcf8e3fd082be7d6a2d1feb9b6fb
anxheloo <anxhelocenollari@gmail.com>
2023-02-23 08:39:54 -0800

    Find the shape of numpy

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -8,10 +8,10 @@ def np_shape(matrix):
     return tuple.shape
 
 
-mat1 = np.array([1, 2, 3, 4, 5, 6])
-mat2 = np.array([])
-mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
+mat1 = [1, 2, 3, 4, 5, 6]
+mat2 = []
+mat3 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]]
 np_shape(mat1)
 np_shape(mat2)
 np_shape(mat3)
