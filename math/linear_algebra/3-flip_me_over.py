ee9208612d277799da2a64cdabff307dde189782
anxheloo <anxhelocenollari@gmail.com>
2023-02-23 06:32:21 -0800

    transpose matrix

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -9,9 +9,7 @@ def matrix_transpose(matrix):
 
 
 mat1 = [[1, 2], [3, 4]]
-print(mat1)
 matrix_transpose(mat1)
 mat2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
         [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
-print(mat2)
 matrix_transpose(mat2)
