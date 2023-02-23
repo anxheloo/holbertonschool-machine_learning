5a2f23cc943b4a822e598903b75c3bfd8765d672
anxheloo <anxhelocenollari@gmail.com>
2023-02-23 03:05:49 -0800

    Siye me up

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,6 +5,8 @@ def matrix_shape(matrix):
         result.append(len(matrix))
         matrix = matrix[0]
     return result
+
+
 mat1 = [[1, 2], [3, 4]]
 matrix_shape(mat1)
 mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
