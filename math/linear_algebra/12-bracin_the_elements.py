71ecf724a8a7d0f2de875a021ed5d5a4105dd989
anxheloo <anxhelocenollari@gmail.com>
2023-02-24 03:54:52 -0800

    Math Operations

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,43 +1,8 @@
 #!/usr/bin/env python3
+'''Math Operations'''
 
-import numpy as np
 
-mat1 = np.array([[11, 22, 33], [44, 55, 66]])
-mat2 = np.array([[1, 2, 3], [4, 5, 6]])
-
-def add(mat1,mat2):
-    a = np.array(mat1)
-    b = np.array(mat2)
-    
-    result = np.add(a,b)
+def np_elementwise(mat1, mat2):
+    '''Math operations function numpy'''
+    result = np.add(mat1, mat2), np.subtract(mat1, mat2), np.multiply(mat1, mat2), np.divide(mat1, mat2)
     return result
-
-
-def sub(mat1,mat2):
-    a = np.array(mat1)
-    b = np.array(mat2)
-    
-    result = np.subtract(a,b)
-    return result
-
-
-def mul(mat1,mat2):
-    a = np.array(mat1)
-    b = np.array(mat2)
-
-    result = np.multiply(a,b)
-    return result
-
-
-def div(mat1,mat2):
-    a = np.array(mat1)
-    b = np.array(mat2)
-    
-    result = np.divide(a,b)
-    return result
-
-
-print(add(mat1,mat2))
-print(sub(mat1,mat2))
-print(mul(mat1,mat2))
-print(div(mat1,mat2))
