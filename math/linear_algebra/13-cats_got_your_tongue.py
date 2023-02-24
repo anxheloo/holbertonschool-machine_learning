50a6e23b5c5ea0f49f216988c8380483abb9c064
anxheloo <anxhelocenollari@gmail.com>
2023-02-24 05:54:23 -0800

    Concatenate matrices

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -7,9 +7,6 @@ def np_cat(mat1,mat2, axis = 0):
     '''done done done'''
     a = np.array(mat1)
     b = np.array(mat2)
-    if axis == 0:
-        c = np.concatenate((a, b), axis=0)
-    else:
-        c = np.concatenate((a, b), axis=1)
+    c = np.concatenate((a, b), axis)
     return c 
 
