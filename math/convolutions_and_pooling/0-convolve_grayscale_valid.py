83fb30b3c894f2002d02cba2949440c18981e2da
anxheloo <99367438+anxheloo@users.noreply.github.com>
2023-03-30 15:19:15 +0200

    Update 0-convolve_grayscale_valid.py

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-"""      """
+"""adffad"""
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 def convolve_grayscale_valid(images, kernel):
-    """        """
+    """def convolve"""
     m, height, width = images.shape
     kh, kw = kernel.shape
-    
     convoluted = np.zeros((m, height - kh + 1, width - kw + 1))
-    
     for h in range(height - kh + 1):
         for w in range(width - kw + 1):
             output = np.sum(images[:, h: h+kh, w: w+kw ] * kernel, axis = 1).sum(axis=1)
