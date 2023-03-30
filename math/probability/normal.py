9c456ec8eab2bed5c06ff770e4da9bf26830adc9
anxheloo <99367438+anxheloo@users.noreply.github.com>
2023-03-30 15:02:33 +0200

    Update normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,38 +1,27 @@
 import numpy as np
-import math
 #!/usr/bin/env python3
 """ defines Normal class that represents normal distribution """
 
+
 class Normal:
     """class that represents normal distribution"""
-
-
     def __init__(self, data=None, mean = 0. , stddev=1.):
         """class constructor"""
-        # write your code here
-        
         self.mean = float(mean)
         self.stddev = float(stddev)
-        
         if stddev <= 0:
             raise ValueError("sttdev must be a positive value")
-            
         if data is None:
             self.mean = mean
             self.stddev = stddev
-            
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-                
+                raise ValueError("data must contain multiple values")   
             self.mean = sum(data) / len(data)
 #             self.stddev = math.sqrt(sum([(x - self.mean) ** 2 for x in data]) / len(data)-1)
             self.stddev = (sum([(x - self.mean) ** 2 for x in data]) / len(data)) ** (1/2)
-            
-    
-
 
     def z_score(self, x):
         """calculates the z-score of a given x-value"""
@@ -62,48 +51,3 @@ class Normal:
         pi = 3.1415926536
         erfx = (2 / (pi**(1/2))) * (x - ((x**3) / 3) + ((x**5) / 10) - ((x**7) / 42) + ((x**9) / 216))
         return erfx
-        
-        
-
-
-np.random.seed(0)
-data = np.random.normal(70, 10, 100).tolist()
-n1 = Normal(data)
-print('Mean:', n1.mean, ', Stddev:', n1.stddev)
-
-n2 = Normal(mean=70, stddev=10)
-print('Mean:', n2.mean, ', Stddev:', n2.stddev)
-
-
-print()
-
-np.random.seed(0)
-data = np.random.normal(70, 10, 100).tolist()
-n1 = Normal(data)
-print('Z(90):', n1.z_score(90))
-print('X(2):', n1.x_value(2))
-
-n2 = Normal(mean=70, stddev=10)
-print()
-print('Z(90):', n2.z_score(90))
-print('X(2):', n2.x_value(2))
-
-
-print()
-
-np.random.seed(0)
-data = np.random.normal(70, 10, 100).tolist()
-n1 = Normal(data)
-print('PSI(90):', n1.pdf(90))
-
-n2 = Normal(mean=70, stddev=10)
-print('PSI(90):', n2.pdf(90))
-
-print()
-np.random.seed(0)
-data = np.random.normal(70, 10, 100).tolist()
-n1 = Normal(data)
-print('PHI(90):', n1.cdf(90))
-
-n2 = Normal(mean=70, stddev=10)
-print('PHI(90):', n2.cdf(90))
