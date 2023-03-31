b5d584b30fc98e343130016b650768be62c135ba
anxheloo <99367438+anxheloo@users.noreply.github.com>
2023-03-31 17:43:31 +0200

    Update exponential.py

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
-"""A class Exponential that represents a Exponential distribution"""
+"""Exponential class for distribution"""
 
 
 class Exponential:
-    """A class Exponential that represents a Exponential distribution"""
+    """Exponential class for distribution"""
+    e = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
         self.lambtha = float(lambtha)
         if self.lambtha <= 0:
-            raise ValueError("lambtha must be a positvice value")
+            raise ValueError("lambtha must be a positive value")
         if data is not None:
             if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(len(data)/sum(data))
+            self.lambtha = 1 / (sum(data) / len(data))
 
-    def pdf(self,x):
-        """A class Exponential that represents a Exponential distribution"""
+    def pdf(self, x):
+        """
+        Probability Density Function for exponential
+        """
         if x < 0:
             return 0
-        e = 2.7182818285
-        pdf = self.lambtha * (e**(-self.lambtha*x))
+        pdf = self.lambtha * Exponential.e ** - (self.lambtha * x)
         return pdf
 
-    def cdf(self,x):
-        """A class Exponential that represents a Exponential distribution"""
+    def cdf(self, x):
+        """
+        Cumulative Distribution Function for exponential
+        """
         if x < 0:
             return 0
-        e = 2.7182818285
-        cdf = 1 - (e**(-self.lambtha*x))
+        cdf = 1 - Exponential.e ** (- self.lambtha * x)
         return cdf
