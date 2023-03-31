38fe8f0cd95afba626e15fd83dd7f5e6a942262e
anxheloo <99367438+anxheloo@users.noreply.github.com>
2023-03-31 17:45:57 +0200

    Update 3-convolve_grayscale.py

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python3
-"""hry"""
+"""Performs a convolution on grayscale images"""
 
 
 import numpy as np
-import matplotlib.pyplot as plt
 
-def convolve_grayscale(images, kernel, padding = 'same', stride=(1,1)):
-    """hlkj"""
+
+def convolve_grayscale(images, kernel, padding="same", stride=(1, 1)):
+    """Performs a convolution on grayscale images"""
+
     m, height, width = images.shape
     kh, kw = kernel.shape
     sh, sw = stride
-    if padding == 'same':
-        if (kh % 2) == 1 and (kw % 2) == 1:
-            ph = (kh - 1) // 2
-            pw = (kw - 1) // 2
-        else:
-            ph = kh // 2
-            pw = kw // 2    
-    elif padding == 'valid':
+
+    if padding == "same":
+        ph = ((height - 1) * sh + kh - height) // 2 + 1
+        pw = ((width - 1) * sw + kw - width) // 2 + 1
+    elif padding == "valid":
         ph = 0
         pw = 0
     else:
         ph, pw = padding
-    images = np.pad(images, ((0,0), (ph,ph),(pw,pw)), 'constant', constant_values=0)
-    ch = ((height + (2*ph) - kh) // sh) + 1
-    cw = ((width + (2*pw) - kw) // sw) + 1  
+
+    p_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)))
+
+    ch = ((height + 2 * ph - kh) // sh) + 1
+    cw = ((width + 2 * pw - kw) // sw) + 1
+
     convoluted = np.zeros((m, ch, cw))
-    i = 0
-    for h in range(0, height + (2*ph) - kh + 1, sh) :
-        j = 0
-        for w in range(0, width + (2*pw) - kw + 1, sw):
-            output = np.sum(images[:, h: h+kh, w: w+kw ] * kernel, axis = 1).sum(axis=1)
-            convoluted[:, i, j] = output
-            j += 1
-        i += 1
+
+    for h in range(ch):
+        for w in range(cw):
+            output = p_images[:, h * sh: h * sh + kh,
+                              w * sw: w * sw + kw] * kernel
+            sum_out = np.sum(output, axis=(1, 2))
+            convoluted[:, h, w] = sum_out
     return convoluted
