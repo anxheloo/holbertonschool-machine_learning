17ff4dbe02d7b88131f8cc84839b4b357a29e670
anxheloo <99367438+anxheloo@users.noreply.github.com>
2023-03-31 17:47:36 +0200

    Update 0-mean_cov.py

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -1,23 +1,20 @@
+#!/usr/bin/env python3
+"""Calculates the mean and covariance of a data set"""
+
+
 import numpy as np
 
+
 def mean_cov(X):
-    #check conditions
-    if not isinstance(X, np.ndarray) or X.ndim !=2:
+    """Calculates the mean and covariance of a data set"""
+    if not isinstance(X, np.ndarray) or (len(X.shape) < 2):
         raise TypeError("X must be a 2D numpy.ndarray")
-    
-    
-    n,d = X.shape
-    
-    if n < 2 : 
+    if len(X) < 2:
         raise ValueError("X must contain multiple data points")
-    
-    
-    mean = np.mean(X, axis = 0)
+    n, d = X.shape
+    mean = np.mean(X, axis=0)
     cov = np.zeros((d, d))
-    
     for i in range(n):
         cov += np.outer((X[i] - mean), (X[i] - mean))
-        
-    cov /= n-1
-    
-    return (mean.reshape(1, d), cov)
+    cov /= n - 1
+    return mean.reshape(1, d), cov
