63a32b6e0786b048c7d7d4e9eeb7eaf749dc43c5
anxheloo <99367438+anxheloo@users.noreply.github.com>
2023-03-31 17:45:17 +0200

    Update 1-convolve_grayscale_same.py

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
-"""afd"""
+"""Performs a same convolution on grayscale images"""
 
 
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 def convolve_grayscale_same(images, kernel):
-    """adfsads"""
+    """Performs a same convolution on grayscale images"""
+
     m, height, width = images.shape
     kh, kw = kernel.shape
-    if (kh % 2) == 1 and (kw % 2) == 1:
-        ph = (kh - 1) // 2
-        pw = kw // 2
-    images = np.pad(images, ((0,0), (ph,ph),(pw,pw)), 'constant', constant_values=0)
+    if kh % 2 != 0 and kw % 2 != 0:
+        padding_height = (kh - 1) // 2
+        padding_width = (kw - 1) // 2
+    else:
+        padding_height = kh // 2
+        padding_width = kw // 2
+
+    p_images = np.pad(images, ((0, 0), (padding_height, padding_height),
+                      (padding_width, padding_width)))
+
     convoluted = np.zeros((m, height, width))
+
     for h in range(height):
         for w in range(width):
-            output = np.sum(images[:, h: h+kh, w: w+kw ] * kernel, axis = 1).sum(axis=1)
-            convoluted[:, h, w] = output
+            output = p_images[:, h: h + kh, w: w + kw] * kernel
+            sum_out = np.sum(output, axis=(1, 2))
+            convoluted[:, h, w] = sum_out
     return convoluted
