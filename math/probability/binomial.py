60829d8764d168dbcf857b90a6361468fc2d0113
anxheloo <99367438+anxheloo@users.noreply.github.com>
2023-03-31 17:43:11 +0200

    Update binomial.py

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -1,47 +1,57 @@
 #!/usr/bin/env python3
-""" defines Normal class that represents normal distribution """
+"""Binomial class for distribution"""
 
 
 class Binomial:
-    """class that represents normal distribution"""
-    def __init__(self, data=None, n=1 , p=0.5):
-        """class constructor""" 
+    """Binomial class for distribution"""
+    e = 2.7182818285
+
+    def __init__(self, data=None, n=1, p=0.5):
         self.n = int(n)
         self.p = float(p)
-        if self.n <= 0:
-            raise ValueError("n must be a positvice value")
-        if p <= 0 or p >=1:
-             raise ValueError("p must be greater than 0 and less than 1")
+        if n < 1:
+            raise ValueError("n must be a positive value")
+        if p <= 0 or p >= 1:
+            raise ValueError("p must be greater than 0 and less than 1")
         if data is not None:
             if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            mean = float(sum(data) / len(data))
-            summation = 0
-            for x in data:
-                summation += ((x - mean) ** 2)
-            variance = (summation / len(data))
-            q = variance / mean
-            p = (1 - q)
-            n = round(mean / p)
-            p = float(mean / n)
-            self.n = n
-            self.p = p
-            
-    def pmf(self,k):
-        """A class Exponential that represents a Exponential distribution"""
-        k = int(k)
-        if k < 0 or k > self.n:
+            mean = sum(data) / len(data)
+            var = sum(map(lambda i: (i - mean) ** 2, data)) / len(data)
+            self.p = 1 - ((var) / mean)
+            self.n = int(round(mean / self.p))
+            self.p = mean / self.n
+
+    def pmf(self, k):
+        """Probability Mass Function for binomial"""
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
             return 0
-        else:
-            return(math.comb(self.n, k) * (self.p ** k) * ((1 - self.p) ** (self.n - k)))
-        
-    def cdf(self,k):
-        k = int(k)
+        n_factorial = 1
+        for i in range(1, self.n + 1):
+            n_factorial *= i
+        x_factorial = 1
+        for i in range(1, k + 1):
+            x_factorial *= i
+        op_factorial = 1
+        for i in range(1, (self.n - k) + 1):
+            op_factorial *= i
+        combinatory = (n_factorial) / (x_factorial * op_factorial)
+        pmf = combinatory * (self.p ** k) * ((1 - self.p) ** (self.n - k))
+        return pmf
+
+    def cdf(self, k):
+        """
+        Cumulative Distribution Function for binomial
+        """
+        if type(k) is not int:
+            k = int(k)
         if k < 0:
             return 0
         cdf = 0
         for i in range(k + 1):
-            cdf += self.pmf(i)
+            cdf += Binomial.pmf(self, i)
         return cdf
