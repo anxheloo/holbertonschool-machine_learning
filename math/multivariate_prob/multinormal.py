1458e61fc2031e8d0311e9c88631bc6fcf1b83b9
anxheloo <99367438+anxheloo@users.noreply.github.com>
2023-03-31 17:48:16 +0200

    Update multinormal.py

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,40 +1,42 @@
+#!/usr/bin/env python3
+"""Initialize Multinormal"""
+
 import numpy as np
-    
+
+
 class MultiNormal:
-    
+    """
+    Multivariate Normal distribution
+    """
+
     def __init__(self, data):
-        
-        if not isinstance(data, np.ndarray) or data.ndim != 2:
+
+        if type(data) != np.ndarray:
             raise TypeError("data must be a 2D numpy.ndarray")
-        
-        n, d = data.shape
-        
+        if len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        d, n = data.shape
         if n < 2:
             raise ValueError("data must contain multiple data points")
-        
-        self.mean = np.mean(data, axis=0).reshape(d, 1)
-        
-        self.cov = np.zeros((d, d))
-        
-        for i in range(d):
-            for j in range(d):
-                self.cov[i][j] = np.sum((data[:,i]-self.mean[i])*(data[:,j]-self.mean[j])) / (n-1)
-                
-                
+        self.mean = np.mean(data, axis=1, keepdims=True)
+        X_mean = data - self.mean
+        self.cov = (X_mean @ X_mean.T) / (n - 1)
 
     def pdf(self, x):
-        
-        if not isinstance(x, np.ndarray):
+        """
+        calculates the PDF at a data point
+        """
+        if type(x) is not np.ndarray:
             raise TypeError("x must be a numpy.ndarray")
-        
-        d, _ = self.mean.shape
-        if x.shape != (d, 1):
-            raise ValueError(f"x must have the shape ({d}, 1)")
-        
+        d = self.cov.shape[0]
+        if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
         det = np.linalg.det(self.cov)
         inv = np.linalg.inv(self.cov)
-        expo = -0.5 * ((x - self.mean).T @ inv @ (x - self.mean))
-        coeff = 1 / (np.sqrt((2*np.pi)**d * det))
-        
-        return coeff * np.exp(expo)
-        
+        first = 1 / ((2 * np.pi) ** (d / 2) * np.sqrt(det))
+        second = np.dot((x - self.mean).T, inv)
+        third = np.dot(second, (x - self.mean) / -2)
+        pdf = first * np.exp(third)
+
+        return pdf[0][0]
