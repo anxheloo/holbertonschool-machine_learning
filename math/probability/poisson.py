b899a853f96ee9f50976a3aa590440815e2eed22
anxheloo <99367438+anxheloo@users.noreply.github.com>
2023-03-31 17:44:12 +0200

    Update poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,37 +1,46 @@
 #!/usr/bin/env python3
-"""A class Poisson that represents a poisson distribution"""
+"""Poisson class for distribution"""
 
 
 class Poisson:
-    """A class Poisson that represents a poisson distribution"""
+    """
+    Poisson class for distribution
+    """
+
     def __init__(self, data=None, lambtha=1.):
         self.lambtha = float(lambtha)
         if self.lambtha <= 0:
             raise ValueError("lambtha must be a positive value")
         if data is not None:
-            if not isinstance(data, list):
+            if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data)) / len(data)
 
-    def pmf(self,k):
-        """A class Poisson that represents a poisson distribution"""
-        if not isinstance(k,int):
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of successes
+        """
+
+        if type(k) is not int:
             k = int(k)
         if (k < 0):
             return 0
         e = 2.7182818285
         lambtha = self.lambtha
-        factorial = 1 
+        factorial = 1
         for i in range(k):
             factorial *= (i + 1)
-        pmf = ((lambtha ** k) * (e** -lambtha)) / factorial
+        pmf = ((lambtha ** k) * (e ** - lambtha)) / factorial
         return pmf
 
-    def cdf(self,k):
-        """A class Poisson that represents a poisson distribution"""
-        if not isinstance(k,int):
+    def cdf(self, k):
+        """
+        Calculates the value of the CDF for a given number of successes
+        """
+
+        if type(k) is not int:
             k = int(k)
         if k < 0:
             return 0
