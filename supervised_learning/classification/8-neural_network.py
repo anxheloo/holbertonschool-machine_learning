6b7f681631321a942e5afe4eb193b0aa8ffff301
anxheloo <99367438+anxheloo@users.noreply.github.com>
2023-04-24 15:26:08 +0200

    Update 8-neural_network.py

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -1,33 +1,35 @@
+#!/usr/bin/env python3
+"""
+Module to create a neural network
+"""
 import numpy as np
 
 
 class NeuralNetwork:
     """
-    class that represents a neural network with one hidden layer
-    performing binary classification
+    A class that defines a neural network with one hidden layer performing
+    binary classification
     """
 
     def __init__(self, nx, nodes):
         """
         class constructor
+        :param nx: is the number of input features to the neuron
         """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
+        self.nx = nx
         if type(nodes) is not int:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-            
-        # nodes number of nodes and receives nx number of input feature
+        # hidden layer
         self.W1 = np.random.randn(nodes, nx)
-        # matches the shape of the hidden layer output 
         self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
-        
-        # only one node and receives the output from the hidden layer as input
+        # output neuron
         self.W2 = np.random.randn(1, nodes)
-        # matching the shape of the output neuron's activation
         self.b2 = 0
         self.A2 = 0
