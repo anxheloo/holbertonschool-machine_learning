6b024d944e8b2cf9172373f67e3265fc4962f2e8
anxheloo <99367438+anxheloo@users.noreply.github.com>
2023-04-24 15:24:43 +0200

    Update 5-neuron.py

--- supervised_learning/classification/5-neuron.py
+++ supervised_learning/classification/5-neuron.py
@@ -1,81 +1,98 @@
+#!/usr/bin/env python3
+"""
+Module to create a neuron
+"""
+import numpy as np
+
+
 class Neuron:
     """
-    class that represents a single neuron performing binary classification
+    A class that defines a single neuron
     """
 
-    # TASK 1
     def __init__(self, nx):
+        """
+        class constructor
+        :param nx: is the number of input features to the neuron
+        """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
+        self.nx = nx
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
-    
-    @property
-    def W(self):
-        return (self.__W)
 
-    @property
-    def b(self):
-        return (self.__b)
-
-    @property
-    def A(self):
-        return (self.__A)
-    
     def forward_prop(self, X):
         """
         calculates the forward propagation of the neuron
+        :param X: a np array with the input data of shape (nx, m)
+        :return: private attribute __A
         """
-        z = np.matmul(self.W, X) + self.b
-        self.__A = 1 / (1 + (np.exp(-z)))
-        return (self.A)
-    
+        preactivation = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-preactivation))
+        return self.__A
+
     def cost(self, Y, A):
         """
-        calculates the cost of the model using logistic regression function:
-            loss = -((Y * log(A)) + ((1 - Y) * log(1 - A)))
-        logistic regression cost function:
-            cost = (1 / m) * sum of loss function for all m example
+        calculates the cost of the model using logistic regression
+        :param Y: a np array with correct labels of shape (1, m)
+        :param A: a np array with the activated output of shape (1, m)
+        :return: the cost
         """
-        m = Y.shape[1]
-        m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
-        cost = (1 / m) * (-(m_loss))
-        return (cost)
-    
+        cost = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = np.sum(cost)
+        cost = - cost / A.shape[1]
+        return cost
+
     def evaluate(self, X, Y):
         """
-        evaluates the neuron's predictions
+        evaluates the neuron prediction
+        :param X: np array with input data of shape (nx, m)
+        :param Y: np array with correct label of shape (1, m)
+        :return: neuron´s prediction and cost of the network
         """
-        A = self.forward_prop(X)
-        cost = self.cost(Y, A)
-        prediction = np.where(A >= 0.5, 1, 0)
-        return (prediction, cost)
-    
+        self.forward_prop(X)
+        prediction = np.where(self.__A >= 0.5, 1, 0)
+        cost = self.cost(Y, self.__A)
+        return prediction, cost
+
     def gradient_descent(self, X, Y, A, alpha=0.05):
         """
         calculates one pass of gradient descent on the neuron
-        derivative of loss function with respect to A:
-            dA = (-Y / A) + ((1 - Y) / (1 - A))
-        derivative of A with respect to z:
-            dz = A * (1 - A)
-        combining two above with chain rule,
-        derivative of loss function with respect to z:
-            dz = A - Y
-        using chain rule with above derivative,
-        derivative of loss function with respect to __W:
-            d__W = Xdz
-        derivative of loss function with respect to __b:
-            d__b = dz
-        one-step of gradient descent updates the attributes with the following:
-            __W = __W - (alpha * d__W)
-            __b = __b - (alpha * d__b)
+        :param X: np array with input data of shape (nx, m)
+        :param Y: np array with correct labels of shape (1, m)
+        :param A: np array with activated output of shape (1, m)
+        :param alpha: the learning rate
+        :return: no return
         """
-        m = Y.shape[1]
-        dz = (A - Y)
-        d__W = (1 / m) * (np.matmul(X, dz.transpose()).transpose())
-        d__b = (1 / m) * (np.sum(dz))
-        self.__W = self.W - (alpha * d__W)
-        self.__b = self.b - (alpha * d__b)
+        dz = A - Y
+        dw = np.matmul(X, dz.T) / A.shape[1]
+        db = np.sum(dz) / A.shape[1]
+        self.__W = self.__W - alpha * dw.T
+        self.__b = self.__b - alpha * db
+
+    @property
+    def W(self):
+        """
+        getter function for W
+        :return: weight vector neuron
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        getter function for b
+        :return: bias for neuron
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        getter function for W
+        :return: activated output of the neuron
+        """
+        return self.__A
